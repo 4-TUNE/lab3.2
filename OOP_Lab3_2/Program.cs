3127eb273899c241ca3b4e7a75ea26ba9058a4f2
4-TUNE <61144719+4-TUNE@users.noreply.github.com>
2020-06-05 11:59:39 +0300

    Добавьте файлы проекта.

---
+++ OOP_Lab3_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace OOP_Lab3_2
+{
+    class Program
+    {
+        static string fileRead = @"111.txt";
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(fileRead);
+            string readNums = reader.ReadToEnd();
+            int[] nums = readNums.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            reader.Close();
+
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            Console.WriteLine($"Sum of all nums is : { sum }");
+            
+        }
+    }
+}
